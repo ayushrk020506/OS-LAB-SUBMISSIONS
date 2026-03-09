b2eca4fc8edbda0d9e0e3c5c06d5d549b884f1e1
ayushrk020506 <ayushkallingal@gmail.com>
2026-03-10 00:22:41 +0530

    Add files via upload

---
+++ WEEK 3/WEEK 3/PRIORITY-NP.c
@@ -0,0 +1,68 @@
+//priority non preemptive
+#include <stdio.h>
+
+int main()
+{
+    int n,i,time=0,completed=0;
+    int at[20],bt[20],pr[20],ct[20],wt[20],tat[20];
+    int done[20]={0};
+
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("AT P%d: ",i+1);
+        scanf("%d",&at[i]);
+
+        printf("BT P%d: ",i+1);
+        scanf("%d",&bt[i]);
+
+        printf("Priority P%d: ",i+1);
+        scanf("%d",&pr[i]);
+    }
+
+    while(completed<n)
+    {
+        int idx=-1;
+
+        for(i=0;i<n;i++)
+        {
+            if(at[i]<=time && done[i]==0)
+            {
+                if(idx==-1 || pr[i]<pr[idx])
+                    idx=i;
+            }
+        }
+
+        if(idx==-1)
+        {
+            time++;
+            continue;
+        }
+
+        time += bt[idx];
+
+        ct[idx]=time;
+        tat[idx]=ct[idx]-at[idx];
+        wt[idx]=tat[idx]-bt[idx];
+
+        done[idx]=1;
+        completed++;
+    }
+
+    printf("\nP\tAT\tBT\tCT\tWT\tTAT\n");
+
+    for (i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
+               i + 1, at[i], bt[i], ct[i], wt[i], tat[i]);
+    }
+         float awt = 0, atat = 0;
+    for (i = 0; i < n; i++) {
+        awt += wt[i];
+        atat += tat[i];
+    }
+
+    printf("\nAverage Waiting Time: %.2f", awt / n);
+    printf("\nAverage Turn-Around Time: %.2f\n", atat / n);
+}
---
+++ WEEK 3/WEEK 3/SJF-P.c
@@ -0,0 +1,69 @@
+//sjf preemptive
+#include <stdio.h>
+
+int main()
+{
+    int n,i,time=0,complete=0;
+    int at[20],bt[20],rt[20],ct[20],wt[20],tat[20];
+
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("AT P%d: ",i+1);
+        scanf("%d",&at[i]);
+
+        printf("BT P%d: ",i+1);
+        scanf("%d",&bt[i]);
+
+        rt[i]=bt[i];
+    }
+
+    while(complete<n)
+    {
+        int idx=-1;
+
+        for(i=0;i<n;i++)
+        {
+            if(at[i]<=time && rt[i]>0)
+            {
+                if(idx==-1 || rt[i]<rt[idx])
+                    idx=i;
+            }
+        }
+
+        if(idx==-1)
+        {
+            time++;
+            continue;
+        }
+
+        rt[idx]--;
+        time++;
+
+        if(rt[idx]==0)
+        {
+            complete++;
+
+            ct[idx]=time;
+            tat[idx]=ct[idx]-at[idx];
+            wt[idx]=tat[idx]-bt[idx];
+        }
+    }
+
+    printf("\nP\tAT\tBT\tCT\tWT\tTAT\n");
+
+    for (i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
+               i + 1, at[i], bt[i], ct[i], wt[i], tat[i]);
+    }
+         float awt = 0, atat = 0;
+    for (i = 0; i < n; i++) {
+        awt += wt[i];
+        atat += tat[i];
+    }
+
+    printf("\nAverage Waiting Time: %.2f", awt / n);
+    printf("\nAverage Turn-Around Time: %.2f\n", atat / n);
+}
